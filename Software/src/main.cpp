7ccca80828f9e14990c307dd4cd0486472aaba62
Baris Dinc <baris.dinc@enevo.com>
2021-02-06 22:05:04 +0200

    initial startup

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -1,9 +1,79 @@
 #include <Arduino.h>
+#include <EEPROMex.h>
+#include "main.h"
+
+
+uint8_t dtmf;
+uint8_t dtmf_;
+
+Repeater_configutarion_t repeater_configuration;
+
+
+
+
+void dtmf_interrupt();
+void init_configuration(Repeater_configutarion_t* config);
+
 
 void setup() {
-  // put your setup code here, to run once:
+    attachInterrupt(0, dtmf_interrupt, FALLING);
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    pinMode(dtmf_pinSTD, INPUT);
+    pinMode(dtmf_pinQ1, INPUT);
+    pinMode(dtmf_pinQ2, INPUT);
+    pinMode(dtmf_pinQ3, INPUT);
+    pinMode(dtmf_pinQ4, INPUT);
+
+    init_configuration(&repeater_configuration);
+
+
+    Serial.begin(9600);
+    Serial.println("hymRC Repeater Contoller (TA7W/OH2UDS-2021)");
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+}
+
+
+/*
+ * Initialize repeater configuration woth default paramaters if no data in EEPROM,
+ * If data in EEPROM then use that information for initialization
+ * input: 
+ * configuration : pointer to repeater configuration to be updated
+ * 
+ * output: 
+ * -
+ */
+void init_configuration(Repeater_configutarion_t* config)
+{
+  // TODO: read default values from EEPROM
+  config->state = REPEATER_ON; 
+  config->beaconmode = BEACON_NONE;
+  config->tx_tail_ms = txtail_ms;
+  config->user_password[0] = 1;
+  config->user_password[1] = 4;
+  config->user_password[2] = 6;
+  config->user_password[3] = 1;
+  config->admin_password[0] = 0;
+  config->admin_password[1] = 6;
+  config->admin_password[2] = 6;
+  config->admin_password[3] = 6;
+  
+}
+
+
+/*
+ * Port Change Interrupt Function
+ * D2 port is the interrupt pin connected to StD output of MT3170/8870 which goes high/low after a DTMF reception
+ * D4 is MT3170/8870 Q1
+ * D5 is MT3170/8870 Q2
+ * D6 is MT3170/8870 Q3
+ * D7 is MT3170/8870 Q4
+ */
+void dtmf_interrupt() {
+  dtmf = (PIND & 0x00F0) >> 4;
+  dtmf_cnt++;
+  Serial.println(dtmf);
 }
---
+++ Software/src/main.h
@@ -0,0 +1,54 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/* Function patamerters */
+#define txtail_ms 1000   // Kuyruk suresi (milisaniye)
+
+
+
+/* Pin Definition */
+#define dtmf_pinSTD 2
+#define dtmf_pinQ1 4
+#define dtmf_pinQ2 5
+#define dtmf_pinQ3 6
+#define dtmf_pinQ4 7
+
+/* Type Definitions */
+typedef enum
+{
+    REPEATER_OFF = 0x00,
+    REPEATER_ON  = 0x01
+} Repeater_state_t;
+
+typedef enum
+{
+    BEACON_NONE  = 0x00,
+    BEACON_MORSE = 0x01,
+    BEACON_AX25  = 0x02,
+    BEACON_BOTH  = 0x03
+} Beaconmode_t;
+
+typedef struct 
+{
+    Repeater_state_t    state;
+    uint16_t            tx_tail_ms;
+    uint8_t             user_password[4];
+    uint8_t             admin_password[4];
+    char                call_sign[6];
+    Beaconmode_t        beaconmode;
+
+} Repeater_configutarion_t;
+
+typedef struct 
+{
+    float               aku_voltaj;
+    uint8_t             fan_durum;
+
+} Beacon_meassage_t;
+
+
+
+
+
+
+#endif /* MAIN_H_ */
